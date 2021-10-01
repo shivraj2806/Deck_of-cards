ab669d6475949a8c75c848a9c44f456f86ebae0f
shivraj2806 <shivjadhav2806@gmail.com>
2021-10-01 09:40:30 +0530

    Added: set up the initial game

--- src/com/bridgelabz/Deck_Of_Cards.java
+++ src/com/bridgelabz/Deck_Of_Cards.java
@@ -1,7 +1,29 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+
 public class Deck_Of_Cards {
-    public static void main(String[] args) {
-        System.out.println("Welcome Message");
+
+    public ArrayList<String> cardDeck = new ArrayList<>();
+
+    public void deckOfCard() {
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        int numOfCards = suits.length * ranks.length;
+        System.out.println("number of cards of " + numOfCards);
+        for (int i = 0; i < ranks.length; i++) {
+            for (int j = 0; j < suits.length; j++) {
+                cardDeck.add( ranks [i] + "----" + suits[j]);
+            }
+        }
+        toPrint(cardDeck);
+    }
+    public static void toPrint(ArrayList<String> cardDeck){
+        System.out.println("cards in Deck:");
+        for (String element : cardDeck){
+            System.out.println(element);
+        }
+        System.out.println();
     }
 }
+
---
+++ src/com/bridgelabz/main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class main {
+    public static void main(String[] args){
+        Deck_Of_Cards deck_of_cards = new Deck_Of_Cards();
+        deck_of_cards.deckOfCard();
+    }
+}
